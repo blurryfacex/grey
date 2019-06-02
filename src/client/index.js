eb3315baafb9f3b96d6c5cccc6fc708c2c04694b
blurryfacex <306965405@qq.com>
2019-06-02 23:48:27 +0800

    once again , fucked

--- src/client/index.js
+++ src/client/index.js
@@ -8,3 +8,41 @@ import ReactGA from 'react-ga'
 import configureStore from '../app/store'
 import createRouter from '../app/router'
 
+
+import * as offlinePluginRuntime from 'offline-plugin'
+if (process.env.NODE_ENV != 'development') {
+  offlinePluginRuntime.install()
+}
+
+(async function() {
+
+  const router = createRouter()
+  const RouterDom = router.dom
+
+  let _route = null
+
+  router.list.some(route => {
+    let match = matchPath(window.location.pathname, route)
+    if (match && match.path) {
+      _route = route
+      return true
+    }
+  })
+
+  await _route.component.preload()
+
+  ReactDOM.hydrate(
+    <Provider>
+      <BrowserRouter>
+        {RouterDom()}
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+  )
+
+  if (process.env.NODE_ENV === 'development') {
+    if (module.hot) {
+      module.hot.accept()
+    }
+  }
+}())
