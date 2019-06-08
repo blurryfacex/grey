34d160d4e014818a3d0bf58469902190660cb44f
blurryfacex <306965405@qq.com>
2019-06-08 16:50:51 +0800

    base

---
+++ src/app/pages/front/home/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+// 壳组件
+import Shell from '@components/shell'
+import Meta from '@components/meta'
+import PostsList from '@components/posts/list'
+
+@Shell
+@withRouter
+export default class Home extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        <Meta title="首页" />
+
+        <PostsList
+          id={'home'}
+          filter={{
+            sort_by: 'create_at',
+            deleted: false,
+            weaken: false
+          }}
+        />
+      </div>
+    )
+  }
+}
